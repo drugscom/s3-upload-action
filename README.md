6776fcf2af3c9ebe5f0e828a3c77b61487c6692e
Andre Sencioles <asenci@gmail.com>
2021-06-02 12:54:25 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# S3 Upload
+
+Upload files to S3 bucket.
+
+## Inputs
+
+### `source`
+
+Source path(s). Default `"."`.
+
+### `recursive`
+
+Copy directories recursively. Default `"true"`.
+
+### `s3-bucket`
+
+Destination S3 bucket.
+
+### `s3-prefix`
+
+Destination bucket key prefix.
+
+## Example usage
+
+```yaml
+uses: drugscom/s3-upload-action@v1
+env:
+  AWS_REGION: us-east-1
+  AWS_ACCESS_KEY_ID: AAAAAAAAAAAAAAA
+  AWS_SECRET_ACCESS_KEY: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+with:
+  s3-bucket: mys3bucket
+```
---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import * as core from '@actions/core'
+import * as fs from 'fs'
+import * as glob from '@actions/glob'
+import * as path from 'path'
+import * as s3 from '@aws-sdk/client-s3'
+import * as utils from '@actions/utils'
+
+async function run(): Promise<void> {
+  try {
+    const source = utils.getInputAsArray('source')
+    const recursive = utils.getInputAsBool('recursive')
+    const s3Bucket = utils.getInputAsString('s3-bucket')
+    const s3Prefix = utils.getInputAsString('s3-prefix')
+
+    const s3Client = new s3.S3Client({})
+
+    const globber = await glob.create(source.join('\n'), {implicitDescendants: recursive})
+    for await (const file of globber.globGenerator()) {
+      if (!utils.fileExist(file)) {
+        core.debug(`Ignoring "${file}": not a file`)
+        continue
+      }
+
+      const s3Key = path.join(s3Prefix, path.basename(file))
+      core.debug(`Copying "${file}" to "s3://${s3Bucket}/${s3Key}"`)
+      try {
+        await s3Client.send(new s3.PutObjectCommand({
+          Bucket: s3Bucket,
+          Key: s3Key,
+          Body: fs.createReadStream(file),
+          ACL: 'public-read',
+        }))
+      } catch (error) {
+        core.setFailed(error.message)
+      }
+    }
+
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+void run()
